d5aeee34f7917eca570b535ebe704bf63cc0e79e
uros87 <uros_brkic@yahoo.com>
2019-06-10 16:41:02 +0200

    ADDED:env variables for Heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-mongoose.connect('mongodb://127.0.0.1:27017/uroseva-baza', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/uroseva-baza', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
